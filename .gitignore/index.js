3aae83c63a8dbfedbddddd6b78ac909e48ca071f
Wys2 <48517965+Wys2@users.noreply.github.com>
2019-03-15 18:33:39 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -20,9 +20,9 @@ client.on("message", message => {
     if (args[0].toLowerCase() === prefix + "mute") {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande :x:")
         let member = message.mentions.members.first()
-        if (!member) return message.channel.send("Membre introuvable")
+        if (!member) return message.channel.send("Membre introuvable ! :x:")
         if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
-        if (member.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition || member.id === message.guild.ownerID) return message.channel.send("Je ne peux pas mute ce membre")
+        if (member.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition || member.id === message.guild.ownerID) return message.channel.send("Je ne peux pas mute ce membre ! :x: ")
         let muterole = message.guild.roles.find(role => role.name === 'Muted')
         if (muterole) {
             member.addRole(muterole)
